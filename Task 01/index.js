07f0f59138f503ab9ec102e2b7d367219da7110b
TosinMary <teetoastl3@gmail.com>
2021-08-20 10:39:06 -0700

    Third Commit

---
+++ Task 01/index.js
@@ -0,0 +1,16 @@
+function Biodata(){
+    let myName = "Bamidele Tosin Mary";
+    let mySex = "Female";
+    let myAge = "24 years";
+    let myMaritalStatus = "Single";
+    let myStateOfOrigin = "Oyo";
+    let myOccupation = "Front End Developer";
+    console.log(myName,);
+    console.log(mySex,); 
+    console.log(myAge,);
+    console.log( myMaritalStatus,);
+    console.log( myStateOfOrigin,);
+    console.log( myOccupation);
+
+}
+Biodata()
